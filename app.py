ca7763c6486ef81d9cd9d03f7b31b21edc387935
ilias <hliaspapoulidis@gmail.com>
2019-10-05 13:44:18 +0100

    1st tutorial commit - strings

---
+++ app.py
@@ -0,0 +1,12 @@
+print('   /|')
+print('  / |')
+print(' /  |')
+print('/___|')
+
+character_name = 'Jonh'
+character_age = '35'
+
+print('There was once a man named ' +character_name+',')
+print('he was '+character_age+' years old.')
+print('He really liked the name '+character_name+ ',')
+print('but didnt like being '+character_age+'.')
